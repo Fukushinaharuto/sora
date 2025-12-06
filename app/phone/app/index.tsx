8ef5013e74046d239fd527bd5bfbe3e8235013ba
haruto8610 <haruto.fukushina0207@gmail.com>
2025-12-06 17:13:11 +0900

    環境構築完了

---
+++ app/phone/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+export default function Index() {
+    return (
+      <View className="flex-1 justify-center items-center bg-sky-50">
+        <Text className="text-3xl font-bold">Home Screen</Text>
+      </View>
+    );
+  }
+  
